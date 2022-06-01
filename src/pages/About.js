0d33fd98f961942cc307dbb0717ba860aa40f117
rakhimjonovjoxongir@gmail.com <jahongir.r1999@gmail.com>
2022-06-02 02:49:29 +0500

    about full

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,40 +5,38 @@ import logo from "../assets/logo.png";
 const About = () => {
   return (
     <div className="portfolio_container d-flex justify-content-center align-items-center">
-       
-          <div className="header_container">
-            <div className="about_title">
-              <h1 style={{ paddingTop: "3rem", margin: "auto 0" }}>
-                <TypeWriterEffect
-                  width="100px"
-                  startDelay={100}
-                  cursorColor="red"
-                  textStyle={{
-                    fontSize: "4rem",
-                    color: "white",
-                    textShadow: "0 0 20px white",
-                  }}
-                  multiTextLoop={true}
-                  multiText={[
-                    "Hi, ",
-                    "My name is Joxongir",
-                    "I'm Fronted Developer ",
-                  ]}
-                  multiTextDelay={1000}
-                  typeSpeed={100}
-                  deleteSpeed={100}
-                  scrollArea="body"
-                />
-              </h1>
-            </div>
-            <div className="d-flex justify-content-center">
-              <div className="box_1">
-                <div className="box_2">
-                  <img className="box_img" src={logo} alt="" />
-                </div>
-              </div>
-            </div>
+      <div className="d-flex justify-content-evenly align-items-center about_container">
+        <div className="about_title">
+          <h1>
+            <TypeWriterEffect
+              width="50%"
+              startDelay={100}
+              cursorColor="red"
+              textStyle={{
+                fontSize: "4rem",
+                color: "white",
+                textShadow: "0 0 20px white",
+              }}
+              multiTextLoop={true}
+              multiText={[
+                "Hi, ",
+                "My name is Joxongir",
+                "I'm Fronted Developer ",
+              ]}
+              multiTextDelay={1000}
+              typeSpeed={100}
+              deleteSpeed={100}
+            />
+          </h1>
         </div>
+        <div className="box_container">
+          <div className="box_1">
+            <div className="box_2">
+              <img className="box_img" src={logo} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
